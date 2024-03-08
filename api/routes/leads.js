cf11b2d9dbce044438a0af1749b0f6347f835d63
rishav143 <rc8344353@gmail.com>
2024-03-08 22:20:43 +0530

    created leads api route

--- api/routes/leads.js
+++ api/routes/leads.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express();
 
-router.get('/', (req, res, next) => {
-    
+router.get('/:info', (req, res, next) => {
+    res.status(200).json({
+        message : 'lead found sucessfully',
+        info : req.params.info
+    });
 });
+
+router.patch('/:status', (req, res, next) => {
+    res.status(200).json({
+        status : 'lead status is '+req.params.status
+    });
+});
+
+module.exports = router;
