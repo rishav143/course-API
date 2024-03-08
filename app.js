f0c57c01ed8356700340c461a71584c009c9daa7
rishav143 <rc8344353@gmail.com>
2024-03-08 23:01:28 +0530

    error handling

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ app.use('/courses', courseRoutes);
 app.use('/leads', leadRoutes);
 app.use('/comments', commentRoutes);
 
+app.use((req, res, next) => {
+    const error = new error('Not found');
+    error.status(404);
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
