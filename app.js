644645f7be0e0840fd3a2565aa4d8f871eea0901
rishav143 <rc8344353@gmail.com>
2024-03-09 17:35:44 +0530

    mongoose added

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const courseRoutes = require('./api/routes/courses');
 const leadRoutes = require('./api/routes/leads');
 const commentRoutes = require('./api/routes/comments');
 
+mongoose.connect(
+    "mongodb+srv://rc8344353:" +
+    process.env.MONGO_ATLAS_PW +
+    "@course-api.wit0kps.mongodb.net/?retryWrites=true&w=majority&appName=course-API"
+)
+
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// to prevent CORS errors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE')
+        return res.status(200).json({});
+    }
+});
 
 //Routes which should handle requests
 app.use('/courses', courseRoutes);
